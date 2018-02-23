ce0232fe2a8280104ac63cbfe46963109a8b1480
Dmitry Safonov <kenderloki@gmail.com>
2018-02-23 18:43:49 +0300

    Implement better content filtering; code cleanup (#2)

--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import "fmt"
 
 func pprint(visitedLinks *map[string]interface{}) {
 	for url, links := range *visitedLinks {
-		fmt.Println(url)
-		for link := range links.(map[string]bool) {
-			fmt.Printf("-- %s\n", link)
+		fmt.Printf("\n%s\n", url)
+		switch links.(type) {
+		case map[string]bool:
+			for link := range links.(map[string]bool) {
+				fmt.Printf("-- %s\n", link)
+			}
+		case string:
+			fmt.Printf("-- %s\n", links)
 		}
 	}
 }
 
 func main() {
+	// TODO: support robots.txt
+	// TODO: set a global crawl timeout using context
+	// TODO: move url, user-agent, timeout, etc. into command-line flags
+	// TODO: improve site map presentation
 	crawler := Crawler{RootURL: "https://monzo.com"}
-	res, _ := crawler.Start()
+	res, err := crawler.Run()
+	if err != nil {
+		fmt.Print(err)
+		panic(err)
+	}
 	pprint(res)
 }
